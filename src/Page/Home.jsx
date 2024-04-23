2f47596138ed1e1dac28d79c9bc8989f5c876e77
Roki <mroki815@gmail.com>
2024-04-23 19:31:39 +0600

    update

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -302,7 +302,7 @@ const Home = () => {
                         <figure><img src="images/service-img2.png"/></figure>
                      </div>
                      <div className="studies_box">
-                        <h3 className="introduction_text">Master of Law, 2012</h3>
+                        <h3 className="introduction_text">Master of Law, 2024</h3>
                      </div>
                      <div className="read_bt active"><a href="#">Read More</a></div>
                   </div>
